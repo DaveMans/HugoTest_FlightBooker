b656fcaca78d80bba8aacd2ac72e12d8f06b9eaf
David Mansilla <you@example.com>
2020-12-03 23:18:03 -0600

    task #3 of 7GUIs challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'flightBookerTest';
+  bookingOptions: string[] = ['one-way flight', 'return flight'];
+
+  selectedOption: string = this.bookingOptions[0];
+  oneWayDate = new Date().toLocaleString();
+  returnDate = new Date().toLocaleString();
+  disableReturn: boolean = true;
+
+  reviewBooking() {
+    let message: string =
+      this.selectedOption === 'one-way flight'
+        ? `You have booked a ${this.selectedOption} for ${this.oneWayDate}`
+        : `You have booked a ${this.selectedOption} for ${this.oneWayDate} to ${this.returnDate}`;
+    alert(message);
+  }
+
+  onChange(value) {
+    value === 'one-way flight'
+      ? (this.disableReturn = true)
+      : (this.disableReturn = false);
+  }
 }
